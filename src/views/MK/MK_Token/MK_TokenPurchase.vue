d6d8f2e09ee6c306b67447131072e97c7d8f45d1
myNameisAkmal <zdiezzystark@gmail.com>
2021-04-21 04:40:27 +0700

    token

--- src/views/MK/MK_Token/MK_TokenPurchase.vue
+++ src/views/MK/MK_Token/MK_TokenPurchase.vue
@@ -7,27 +7,38 @@
               <b-col lg="12" xl="12">
                 <b-row>
                   <b-col lg="2" xl="2" style="margin-right: 20px;">
-                    <b-img :src="urlHoonian + ''" alt="" style="height: 90px;" fluid-grow rounded @error="onImageLoadFailure($event)" />
+                    <b-img :src="urlHoonian + ModelDetail.icon_project" alt="" style="height: 90px;" fluid-grow rounded @error="onImageLoadFailure($event)" />
                   </b-col>
-                  <b-col lg="4" xl="4" style="margin-right: 20px;">
-                    <label style="font-weight: bold; font-size: 20px; margin-bottom: 10px;" class="title-primary">
-                      {{ModelDetail.project_name}}
-                    </label> <br>
-                    <label style="font-weight: bold; font-size: 20px; margin-bottom: 10px;" class="lbl-poppins">{{$t('total_owned_token')}}</label> <br>
-                    <label style="font-weight: bolder; font-size: 20px;" class="title-primary">
-                      {{ModelDetail.total_owned_token}}
-                    </label>
-                  </b-col>
-                  <b-col lg="4" xl="4" style="margin-right: 20px; position: relative;">
-                    <!-- <span style="font-weight: bolder; font-size: 20px; margin-bottom: 10px;" class="title-primary">
-                      {{ModelDetail.project_name}}
-                    </span> <br> -->
-                    <div style="position: absolute; bottom: 0;">
-                      <label style="font-weight: bold; font-size: 20px; margin-bottom: 10px;" class="lbl-poppins">{{$t('token_value')}}</label> <br>
-                      <span style="font-weight: bolder; font-size: 20px;" class="title-primary">
-                        {{ModelDetail.token_value_project}}
-                      </span>
-                    </div>
+                  <b-col>
+                    <b-row>
+                      <b-col>
+                        <label style="font-weight: bold; font-size: 20px; height: 110px;" class="title-primary">
+                          {{ModelDetail.project_name}}
+                        </label>
+                      </b-col>
+                    </b-row>
+                    <b-row>
+                      <b-col lg="4" xl="4" style="margin-right: 20px;">
+                        <div style="position: absolute; bottom: 0;">
+                          
+                          <label style="font-weight: bold; font-size: 20px; margin-bottom: 10px;" class="lbl-poppins">{{$t('total_owned_token')}}</label> <br>
+                          <label style="font-weight: bolder; font-size: 20px;" class="title-primary">
+                            {{ModelDetail.total_owned_token}}
+                          </label>
+                        </div>
+                      </b-col>
+                      <b-col lg="4" xl="4" style="margin-right: 20px;">
+                        <!-- <span style="font-weight: bolder; font-size: 20px; margin-bottom: 10px;" class="title-primary">
+                          {{ModelDetail.project_name}}
+                        </span> <br> -->
+                        <div style="position: absolute; bottom: 0;">
+                          <label style="font-weight: bold; font-size: 20px; margin-bottom: 10px;" class="lbl-poppins">{{$t('token_value')}}</label> <br>
+                          <span style="font-weight: bolder; font-size: 20px;" class="title-primary">
+                            {{ModelDetail.token_value_project}}
+                          </span>
+                        </div>
+                      </b-col>
+                    </b-row>
                   </b-col>
                 </b-row>
                 <div class="card">
@@ -44,7 +55,7 @@
                                     <label style="font-weight: bold; font-size: 20px;" class="lbl-poppins">{{$t('token_value')}}@</label>
                                   </span> <br>
                                   <span>
-                                    <label style="font-weight: bold; font-size: 20px;" class="title-primary">{{ModelDetail.token_value}}</label>
+                                    <label style="font-weight: bold; font-size: 20px;" class="title-primary">{{ModelDetail.token_value_s}}</label>
                                   </span>
                               </b-col>
                               <b-col md="3">
@@ -53,6 +64,7 @@
                                   </span>
                                   <ACCTextBox
                                     :prop="PI_total_purchase"
+                                    @input="OnPurchaseChange"
                                     v-model="Model.total_purchase"
                                     ref="ref_total_purchase"
                                   />
@@ -101,6 +113,7 @@ export default {
   data() {
     return {
       ModelDetail: {
+        icon_project: "",
         project_name: "Adhi City",
         total_owned_token: 12,
         token_value_project: "75,000,000",
@@ -127,6 +140,11 @@ export default {
     onImageLoadFailure(event) {
       event.target.src = require("@/assets/logo_hoonian1.svg");
     },
+    OnPurchaseChange(data) {
+      let i = data && data !== '' ? parseInt(data) : 0;
+      let x = i * this.ModelDetail.token_value;
+      this.ModelDetail.purchase_value = this.isCurrency(x, 0);
+    },
     doBack() {
       this.$router.go(-1);
     },
@@ -134,6 +152,21 @@ export default {
       this.Model = {
         total_purchase: ""
       };
+    },
+    getDataBy() {
+      let data = this.paramFromList;
+      console.log(data)
+      this.ModelDetail = {
+        icon_project: data.icon_project,
+        project_name: data.project_name,
+        total_owned_token: data.total_token ? data.total_token : 0,
+        token_value_project: this.isCurrency( ((data.total_token ? data.total_token : 0) * (data.token_value ? data.token_value : 0)) , 0),
+        token_value: data.token_value ? data.token_value : 0,
+        token_value_s: this.isCurrency(data.token_value, 0),
+        purchase_value: 0,
+      }
+
+      this.$forceUpdate();
     },
     doSave() {
       this.$validator._base.validateAll("FormEntry").then((result) => {
@@ -151,20 +184,20 @@ export default {
     M_Save() {
       let param = {
         project_id: this.paramFromList.project_id,
-        sales_lead_id: this.paramFromList.sales_lead_id,
-        marketing_agent_id: this.getDataUser().marketing_id,
-        marketing_agent_name: this.getDataUser().user_name,
-        description: this.Model.description,
-        refer_to: this.Model.refer_to,
-        stop_followup: this.Model.stop_followup == "Y"
+        marketing_id: this.getDataUser().marketing_id,
+        principle_id: this.getDataUser().principle_id,
+        qty: this.Model.total_purchase,
+        price: this.ModelDetail.token_value,
+        total_price: this.replaceAllString(this.ModelDetail.purchase_value, ',', '', 'number'),
       }
-      this.postJSON(this.urlHoonian + '/api/marketing-website/lead/logbook/add', param).then((response) => {
+      this.postJSON(this.urlHoonian + '/api/marketing-website/token/purchase', param).then((response) => {
         if (response == null) return;
         this.doBack();
       });
     },
   },
   mounted() {
+    this.getDataBy();
   },
 };
 </script>
