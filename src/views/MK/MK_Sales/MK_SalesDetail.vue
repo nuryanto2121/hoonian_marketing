cb8a51a9564135d330b28b7ab503ce685df7a239
Danniel Agus Wahyudi <dannielaguswahyudi@gmail.com>
2021-05-17 11:13:31 +0700

    add IDR

--- src/views/MK/MK_Sales/MK_SalesDetail.vue
+++ src/views/MK/MK_Sales/MK_SalesDetail.vue
@@ -80,7 +80,7 @@
                   <label class="lbl-poppins">{{ $t('price') }}</label>
                 </b-col>
                 <b-col style="color: #4A93B3;">
-                  {{ isCurrency(dataBuyerDetail.info.price, 0) }}
+                  IDR {{ isCurrency(dataBuyerDetail.info.price, 0) }}
                 </b-col>
               </b-row>
               <b-row>
@@ -99,7 +99,7 @@
                   <label class="lbl-poppins">{{ $t('booking_fee') }}</label>
                 </b-col>
                 <b-col style="color: #4A93B3;">
-                  {{ isCurrency(dataBuyerDetail.info.booking_fee, 0) }}
+                  IDR {{ isCurrency(dataBuyerDetail.info.booking_fee, 0) }}
                 </b-col>
               </b-row>
               <b-row>
@@ -123,7 +123,7 @@
                   <label class="lbl-poppins">{{ $t('commission') }}</label>
                 </b-col>
                 <b-col style="color: #4A93B3;">
-                  {{ isCurrency(dataBuyerDetail.info.marketing_commission, 0) }}
+                  IDR {{ isCurrency(dataBuyerDetail.info.marketing_commission, 0) }}
                 </b-col>
               </b-row>
               <b-row>
