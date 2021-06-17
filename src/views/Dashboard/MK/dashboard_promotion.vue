a71fa4f8d0187a457c926c02d5fd140cd00bb26c
myNameisAkmal <zdiezzystark@gmail.com>
2021-06-17 13:05:28 +0700

    dashboard promotion

--- src/views/Dashboard/MK/dashboard_promotion.vue
+++ src/views/Dashboard/MK/dashboard_promotion.vue
@@ -10,15 +10,61 @@
         </span>
       </b-col>
     </b-row>
-    <b-row v-if="Model.length > 0" style="margin-top: 10px;">
+    <b-row v-if="Model.length > 0" style="padding-top: 10px; background: #F8F8F8; padding-bottom: 20px;">
+      <template v-for="(data, index) in Model">
+        <b-col md="3" v-bind:key="index" :style="`box-shadow: 2px 6px 11px -7px black; border-radius: 6px;
+          -webkit-box-flex: 0;
+          flex: 0 0 21%;
+          padding: 0px !important;
+          max-width: 21%;` + (index < Model.length-1 ? 'margin-right: 5%;' : '')
+        "  @click="doViewDetail(data)">
+          <b-row>
+            <b-col style="height: 18vw; padding: 0px !important;">
+              <b-img :src="urlHoonian + data.picture" alt="" class="center" style="max-height: 100%;" fluid-grow rounded @error="onImageLoadFailure($event)" />
+              <span class="highlight-project">
+                {{data.project_name}}
+              </span>
+              <!-- <div style="position: absolute;
+                top: 18px;
+                right: 0%;
+                background: #52A0FC;
+                border-radius: 5px;
+                width: 100px;
+                padding: 5px;
+                text-align: center;
+                font-size: 14px;
+                color: white;">
+                <b>{{data.total_buyer}} Buyers</b>
+              </div> -->
+            </b-col>
+          </b-row>
+          <b-row style="padding-left: 10px;">
+            <b-col style="font-size: 15px; font-weight: bold;">
+              {{cutWord(data.promotion_name, 20)}}
+            </b-col>
+          </b-row>
+          <!-- <b-row>
+            <b-col style="font-size: 12px; color: #52A0FC;">
+              {{cutWord(data.project_name,25)}}
+            </b-col>
+          </b-row> -->
+          <b-row style="padding-left: 10px; padding-bottom: 5px;">
+            <b-col style="font-size: 12px; color: #616161;">
+              {{momentUnix(data.start_datetime, "MMM YYYY")}} - {{momentUnix(data.end_datetime, "MMM YYYY")}}
+            </b-col>
+          </b-row>
+        </b-col>
+      </template>
+    </b-row>
+    <!-- <b-row v-if="Model.length > 0" style="margin-top: 10px;">
       <b-col style="overflow-x: auto; white-space: nowrap; display: block !important;">
         <template v-for="(item, index) in Model">
           <div class="card" v-bind:key="index" style="display: inline-block !important; width: 35%; margin-right: 10px;">
             <div class="card__body" style="padding: unset !important;">
               <b-row>
                 <b-col style="padding: unset !important;">
-                  <b-img :src="urlHoonian + item.picture" alt=""
-                  :style="`height: 160px; cursor: pointer; position: relative;`"
+                  <b-img :src="urlHoonian + item.picture" alt="" class="img-h"
+                  :style="`cursor: pointer; position: relative;`"
                   fluid-grow @error="onImageLoadFailure($event)" @click="doViewDetail(item)" />
                   <span style="position: absolute; width: 87%; left: 20px; top: 10px; background: rgba(0, 0, 0, 0.3); color: white; padding: 5px 10px; font-size: 12px;">
                     {{item.project_name}}
@@ -39,7 +85,7 @@
           </div>
         </template>
       </b-col>
-    </b-row>
+    </b-row> -->
   </div>
 </template>
 
@@ -87,4 +133,40 @@ export default {
    -webkit-line-clamp: 3; /* number of lines to show */
    -webkit-box-orient: vertical;
 }
+
+@media (min-width: 992px) {
+  .img-h {
+    height: 170px;
+  }
+
+  .highlight-project {
+    position: absolute;
+    width: 87%;
+    left: 50%;
+    top: 20%;
+    background: rgba(0, 0, 0, 0.3);
+    color: white;
+    padding: 5px 10px;
+    font-size: 12px;
+    transform: translate(-50%, -50%);
+  }
+}
+@media (min-width: 1200px) {
+  .img-h {
+    height: 240px;
+  }
+  
+  .highlight-project {
+    position: absolute;
+    width: 87%;
+    left: 50%;
+    top: 10%;
+    background: rgba(0, 0, 0, 0.3);
+    color: white;
+    padding: 5px 10px;
+    font-size: 12px;
+    transform: translate(-50%, -50%);
+  }
+}
+
 </style>
