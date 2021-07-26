2673ee08cd6d0a1a81d5d1283b147813b9b823e6
myNameisAkmal <zdiezzystark@gmail.com>
2021-07-27 01:05:46 +0700

    login

--- src/views/Authentication/SignIn/SignInMobile.vue
+++ src/views/Authentication/SignIn/SignInMobile.vue
@@ -1,148 +1,145 @@
 <template>
   <div class="abs-login">
-    <div class="abs-login__box-position-left" style="background: rgb(74, 147, 179);">
+    <div class="abs-login__box-position-left bgc-login">
       <div class="box-layer-form" style="width:100%; height: 70%; margin-right: 20px; top: 100px; position: absolute;">
-        <div class="row" style="height: 100%;">
-          <!-- <div class="col">
-            
-          </div> -->
-          <template class="display-only-landscape">
-            <div class="col" style="background-color: #FFFF; border-bottom-right-radius: 50%;">
-              <div class="box-layer-form">
-                <div class="box-login-form">
-                  <b-form @submit.prevent="onSubmit">
-                    <div class="form-label-group text-field-position">
-                      <input
-                        v-model="userName"
-                        type="text"
-                        id="inputUsername"
-                        class="form-control clear-input"
-                        placeholder="Username"
-                        autofocus
-                        style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
-                        required
-                      />
-                      <span @click="removeUserName">&times;</span>
-                    </div>
-                    <div class="form-label-group text-field-position">
-                      <input
-                        v-model="passWord"
-                        type="password"
-                        id="inputPassword"
-                        class="form-control clear-input"
-                        placeholder="Password"
-                        autocomplete="off"
-                        style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
-                        required
-                      />
-                      <span @click="removePassword">&times;</span>
-                    </div>
-                    <div v-if="captchaTxt">
-                      <label
-                        style="text-align: center; width: 100%; background-color: cadetblue; border-radius: 5px;"
-                        for="captcha"
-                      >{{captcha}}</label>
-                      <input
-                        v-model="captCha"
-                        type="text"
-                        id="inputCaptcha"
-                        class="form-control"
-                        placeholder="input captcha"
-                        autocomplete="off"
-                        style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
-                        required
-                      />
-                    </div>
-                    <button
-                      class="btn btn-big btn-block"
-                      style="background-color: rgb(74, 147, 179);height:50px;color:white;border-radius:8px;font-size:14px !important;margin-bottom:5px;"
-                      type="submit"
-                    >Sign In</button>
-                    <div class="forgot-password" style="text-align:center;">
-                      <label
-                        @click="showForForgetPassword"
-                        for
-                        style="font-size:14px !important;color: rgb(74, 147, 179); cursor: pointer !important;"
-                      >Forget Password ?</label>
-                    </div>
-                  </b-form>
-                </div>
-              </div>
-            </div>
-          </template>
-          <template class="display-only-portrait">
-            <div class="col" style="background-color: #FFFF; border-bottom-right-radius: 100px; padding-top: 50px;">
-              <div class="box-layer-form">
-                  <div style="text-align: center; width: 100%">
-                      <img :src="require('@/assets/logo_hoonian2.svg')" alt style="width: 90%;" />
+        <div class="row" style="height: 100%; margin-left: 20px !important; margin-right: 20px !important;">
+          <div class="col display-only-landscape">
+            <img :src="require('@/assets/logo_hoonian2.svg')" alt style="width: 100%; top: 40% !important;" class="center" />
+          </div>
+          <div class="col display-only-landscape" style="background-color: #FFFF; border-bottom-right-radius: 100px;">
+            <div class="box-layer-form">
+              <div class="box-login-form center" style="width: 70%;">
+                <b-form @submit.prevent="onSubmit">
+                  <div class="form-label-group text-field-position">
+                    <input
+                      v-model="userName"
+                      type="text"
+                      id="inputUsername"
+                      class="form-control clear-input"
+                      placeholder="Username"
+                      autofocus
+                      style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
+                      required
+                    />
+                    <span @click="removeUserName">&times;</span>
                   </div>
-                <div class="box-login-form" style="margin: 20px 25px 0 25px;">
-                  <b-form @submit.prevent="onSubmit">
-                  <span>
-                      <label class="lbl-poppins">{{ $t('username') }}</label>
-                  </span>
-                    <div class="form-label-group text-field-position">
-                      <input
-                        v-model="userName"
-                        type="text"
-                        id="inputUsername"
-                        class="form-control clear-input"
-                        placeholder="Username"
-                        autofocus
-                        style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
-                        required
-                      />
-                      <span @click="removeUserName">&times;</span>
-                    </div>
-                    <span>
-                      <label class="lbl-poppins">{{ $t('password') }}</label>
-                  </span>
-                    <div class="form-label-group text-field-position">
-                      <input
-                        v-model="passWord"
-                        type="password"
-                        id="inputPassword"
-                        class="form-control clear-input"
-                        placeholder="Enter Password"
-                        autocomplete="off"
-                        style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
-                        required
-                      />
-                      <span @click="removePassword">&times;</span>
-                    </div>
-                    <div v-if="captchaTxt">
-                      <label
-                        style="text-align: center; width: 100%; background-color: cadetblue; border-radius: 5px;"
-                        for="captcha"
-                      >{{captcha}}</label>
-                      <input
-                        v-model="captCha"
-                        type="text"
-                        id="inputCaptcha"
-                        class="form-control"
-                        placeholder="input captcha"
-                        autocomplete="off"
-                        style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
-                        required
-                      />
-                    </div>
-                    <button
-                      class="btn btn-big btn-block"
-                      style="background-color: rgb(74, 147, 179);height:50px;color:white;border-radius:8px;font-size:18px !important;margin-bottom:15px;font-weight: bold;"
-                      type="submit"
-                    >Login</button>
-                    <div class="forgot-password" style="text-align:center;">
-                      <label
-                        @click="showForForgetPassword"
-                        for
-                        style="font-size:14px !important;color: rgb(74, 147, 179); cursor: pointer !important;"
-                      >Forgot Password</label>
-                    </div>
-                  </b-form>
-                </div>
+                  <div class="form-label-group text-field-position">
+                    <input
+                      v-model="passWord"
+                      type="password"
+                      id="inputPassword"
+                      class="form-control clear-input"
+                      placeholder="Password"
+                      autocomplete="off"
+                      style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
+                      required
+                    />
+                    <span @click="removePassword">&times;</span>
+                  </div>
+                  <div v-if="captchaTxt">
+                    <label
+                      style="text-align: center; width: 100%; background-color: cadetblue; border-radius: 5px;"
+                      for="captcha"
+                    >{{captcha}}</label>
+                    <input
+                      v-model="captCha"
+                      type="text"
+                      id="inputCaptcha"
+                      class="form-control"
+                      placeholder="input captcha"
+                      autocomplete="off"
+                      style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
+                      required
+                    />
+                  </div>
+                  <button
+                    class="btn btn-big btn-block"
+                    style="background-color: rgb(74, 147, 179);height:50px;color:white;border-radius:8px;font-size:14px !important;margin-bottom:5px;"
+                    type="submit"
+                  >Sign In</button>
+                  <div class="forgot-password" style="text-align:center;">
+                    <label
+                      @click="showForForgetPassword"
+                      for
+                      style="font-size:14px !important;color: rgb(74, 147, 179); cursor: pointer !important;"
+                    >Forget Password ?</label>
+                  </div>
+                </b-form>
               </div>
             </div>
-          </template>
+          </div>
+
+          <div class="col display-only-potrait" style="background-color: #FFFF; border-bottom-right-radius: 100px; padding-top: 50px;">
+            <div class="box-layer-form">
+                <div style="text-align: center; width: 100%">
+                    <img :src="require('@/assets/logo_hoonian2.svg')" alt style="width: 90%;" />
+                </div>
+              <div class="box-login-form" style="margin: 20px 25px 0 25px;">
+                <b-form @submit.prevent="onSubmit">
+                <span>
+                    <label class="lbl-poppins">{{ $t('username') }}</label>
+                </span>
+                  <div class="form-label-group text-field-position">
+                    <input
+                      v-model="userName"
+                      type="text"
+                      id="inputUsername"
+                      class="form-control clear-input"
+                      placeholder="Username"
+                      autofocus
+                      style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
+                      required
+                    />
+                    <span @click="removeUserName">&times;</span>
+                  </div>
+                  <span>
+                    <label class="lbl-poppins">{{ $t('password') }}</label>
+                </span>
+                  <div class="form-label-group text-field-position">
+                    <input
+                      v-model="passWord"
+                      type="password"
+                      id="inputPassword"
+                      class="form-control clear-input"
+                      placeholder="Enter Password"
+                      autocomplete="off"
+                      style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
+                      required
+                    />
+                    <span @click="removePassword">&times;</span>
+                  </div>
+                  <div v-if="captchaTxt">
+                    <label
+                      style="text-align: center; width: 100%; background-color: cadetblue; border-radius: 5px;"
+                      for="captcha"
+                    >{{captcha}}</label>
+                    <input
+                      v-model="captCha"
+                      type="text"
+                      id="inputCaptcha"
+                      class="form-control"
+                      placeholder="input captcha"
+                      autocomplete="off"
+                      style="height:50px;border-radius:8px !important;font-size:14px !important;font-weight:400;margin-bottom:12px;"
+                      required
+                    />
+                  </div>
+                  <button
+                    class="btn btn-big btn-block"
+                    style="background-color: rgb(74, 147, 179);height:50px;color:white;border-radius:8px;font-size:18px !important;margin-bottom:15px;font-weight: bold;"
+                    type="submit"
+                  >Login</button>
+                  <div class="forgot-password" style="text-align:center;">
+                    <label
+                      @click="showForForgetPassword"
+                      for
+                      style="font-size:14px !important;color: rgb(74, 147, 179); cursor: pointer !important;"
+                    >Forgot Password</label>
+                  </div>
+                </b-form>
+              </div>
+            </div>
+          </div>
           <!-- <div class="column-left">
             <img :src="require('@/assets/logo_hoonian2.svg')" alt style="width: 450px;" />
           </div> -->
@@ -422,4 +419,23 @@ export default {
   font-weight: bold;
   color: rgb(74, 147, 179);
 }
+
+/* @media all and (orientation:landscape) {
+  .display-only-potrait {
+    display: none;
+  }
+
+  .display-only-landscape {
+    display: block;
+  }
+}
+@media all and (orientation:portrait) {
+  .display-only-potrait {
+    display: block;
+  }
+
+  .display-only-landscape {
+    display: none;
+  }
+} */
 </style>
--- src/views/Dashboard/MK_Common/dashboard_news_mobile.vue
+++ src/views/Dashboard/MK_Common/dashboard_news_mobile.vue
@@ -5,85 +5,88 @@
         <div style="padding-left: 5px; text-shadow: 0.5px 0px; font-size: 22px; margin-bottom: 10px;">
           {{ $t('news') }}
         </div>
-        <template v-for="(news, index) in Model">
-          <b-row :key="index" :style="index > 0 ? 'margin-top: 20px;' : ''">
+        
+            <b-row>
+            <template v-for="(news, index) in Model">
+              <!-- <b-row :key="index" :style="index > 0 ? 'margin-top: 20px;' : ''"> -->
 
-            <b-col cols="4" style="" @click="showWebsite(Model[index])">
-                <b-img :src="urlHoonian + Model[index].main_image" alt=""
-                :style="`height: 100px; cursor: pointer;`"
-                fluid-grow @error="onImageLoadFailure($event)" />
-            </b-col>
-            <b-col  cols="8" style="" align-self="center">
-                <b-row>
-                    <b-col style="color: #4A93B3; font-size: 15px;" class="text">
-                        {{Model[index].title}}
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col style="color: #BDBDBD; font-size: 12px;">
-                        {{momentUnix(Model[index].created_at, "DD MMM YYYY")}}
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col style="color: #BDBDBD; font-size: 12px;">
-                        {{getDomainName(Model[index].url)}}
-                    </b-col>
-                </b-row>
-            </b-col>
+              <b-col cols="4" style="" @click="showWebsite(Model[index])" class="news-img">
+                  <b-img :src="urlHoonian + Model[index].main_image" alt=""
+                  :style="`height: 100px; cursor: pointer;`"
+                  fluid-grow @error="onImageLoadFailure($event)" />
+              </b-col>
+              <b-col cols="8" style="" align-self="center" class="news-card">
+                  <b-row>
+                      <b-col style="color: #4A93B3; font-size: 15px;" class="text">
+                          {{Model[index].title}}
+                      </b-col>
+                  </b-row>
+                  <b-row>
+                      <b-col style="color: #BDBDBD; font-size: 12px;">
+                          {{momentUnix(Model[index].created_at, "DD MMM YYYY")}}
+                      </b-col>
+                  </b-row>
+                  <b-row>
+                      <b-col style="color: #BDBDBD; font-size: 12px;">
+                          {{getDomainName(Model[index].url)}}
+                      </b-col>
+                  </b-row>
+              </b-col>
 
-            <!-- left -->
-            <!-- <template v-if="index % 2 == 0">
-              <b-col sm="2" style="" @click="showWebsite(Model[index])">
-                <b-img :src="urlHoonian + Model[index].main_image" alt=""
-                :style="`height: 100px; cursor: pointer;`"
-                fluid-grow @error="onImageLoadFailure($event)" />
-              </b-col>
-              <b-col  sm="4" style="" align-self="center">
-                <b-row>
-                  <b-col style="color: #4A93B3" class="text">
-                    {{Model[index].title}}
-                  </b-col>
-                </b-row>
-                <b-row>
-                  <b-col style="color: #BDBDBD;">
-                    {{momentUnix(Model[index].created_at, "DD MMM YYYY")}}
-                  </b-col>
-                </b-row>
-                <b-row>
-                  <b-col style="color: #BDBDBD;">
-                    {{getDomainName(Model[index].url)}}
-                  </b-col>
-                </b-row>
-              </b-col>
-            </template> -->
-          
-          <!-- right -->
-          <!-- <template v-if="index % 2 == 0 && Model.length > index + 1">
-            <b-col  sm="2" style="" @click="showWebsite(Model[index + 1])">
-              <b-img :src="urlHoonian + Model[index + 1].main_image" alt=""
-              :style="`height: 100px; cursor: pointer;`"
-              fluid-grow @error="onImageLoadFailure($event)" />
-            </b-col>
-            <b-col  sm="4" style="" align-self="center">
-              <b-row>
-                <b-col style="color: #4A93B3" class="text">
-                  {{Model[index + 1].title}}
+                <!-- left -->
+                <!-- <template v-if="index % 2 == 0">
+                <b-col  sm="2" style="" @click="showWebsite(Model[index])">
+                  <b-img :src="urlHoonian + Model[index].main_image" alt=""
+                  :style="`height: 100px; cursor: pointer;`"
+                  fluid-grow @error="onImageLoadFailure($event)" />
                 </b-col>
-              </b-row>
-              <b-row>
-                <b-col style="color: #BDBDBD;">
-                  {{momentUnix(Model[index + 1].created_at, "DD MMM YYYY")}}
+                <b-col  sm="4" style="" align-self="center">
+                  <b-row>
+                    <b-col style="color: #4A93B3">
+                      {{Model[index].title}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col style="color: #BDBDBD;">
+                      {{momentUnix(Model[index].created_at, "DD MMM YYYY")}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col style="color: #BDBDBD;">
+                      {{getDomainName(Model[index].url)}}
+                    </b-col>
+                  </b-row>
                 </b-col>
-              </b-row>
-              <b-row>
-                <b-col style="color: #BDBDBD;">
-                  {{getDomainName(Model[index + 1].url)}}
+                </template> -->
+              
+              <!-- right -->
+              <!-- <template v-if="index % 2 == 0 && News.length > index + 1">
+                <b-col  sm="2" style="" @click="showWebsite(Model[index + 1])">
+                  <b-img :src="urlHoonian + Model[index + 1].main_image" alt=""
+                  :style="`height: 100px; cursor: pointer;`"
+                  fluid-grow @error="onImageLoadFailure($event)" />
                 </b-col>
-              </b-row>
-            </b-col>
-            </template> -->
-          </b-row>
-        </template>
+                <b-col  sm="4" style="" align-self="center">
+                  <b-row>
+                    <b-col style="color: #4A93B3">
+                      {{Model[index + 1].title}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col style="color: #BDBDBD;">
+                      {{momentUnix(Model[index + 1].created_at, "DD MMM YYYY")}}
+                    </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col style="color: #BDBDBD;">
+                      {{getDomainName(Model[index + 1].url)}}
+                    </b-col>
+                  </b-row>
+                </b-col>
+                </template> -->
+              <!-- </b-row> -->
+            </template>
+            </b-row>
       </b-col>
     </b-row>
     <ABSModalImage id="Modal_Image" ref="Modal_Image" size="md" />
